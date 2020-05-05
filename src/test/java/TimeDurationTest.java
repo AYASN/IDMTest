5b0c4e521d3554aa267d4cfb18ebf4bc190e63e3
AYassine <assiryassine@gmail.com>
2020-05-05 17:23:40 +0200

    Add tests on BadException

--- src/test/java/TimeDurationTest.java
+++ src/test/java/TimeDurationTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class TimeDurationTest {
 
@@ -33,7 +34,11 @@ public class TimeDurationTest {
         assertThat(result).isEqualTo(expected);
     }
 
-
-
+    @Test
+    public void should_raise_exception_when_given_negative_integer() {
+        assertThatExceptionOfType(BadBadValueException.class)
+                .isThrownBy(() -> new TimeDuration(-741).toString())
+                .withMessage("The input should be a positive integer!");
+    }
 
 }
